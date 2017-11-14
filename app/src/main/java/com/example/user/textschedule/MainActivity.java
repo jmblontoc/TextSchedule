d94d692f652e5424deb862b89c03bdf8c77fc507
paolomanlapaz <32410303+paolomanlapaz@users.noreply.github.com>
2017-11-14 12:51:16 +0800

    Added add button on activity_main

--- app/src/main/java/com/example/user/textschedule/MainActivity.java
+++ app/src/main/java/com/example/user/textschedule/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.user.textschedule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvTexts;
+    TextView tvSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvTexts = (RecyclerView) findViewById(R.id.rv_text);
+
+        //insert data on Recycler View
     }
 }
