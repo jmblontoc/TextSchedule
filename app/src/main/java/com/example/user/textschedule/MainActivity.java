336c6c01afc91d405caead1b3437428dff9e65f6
charlespsng18 <charlespsng18@yahoo.com>
2017-11-15 09:23:58 +0800

    temporary

--- app/src/main/java/com/example/user/textschedule/MainActivity.java
+++ app/src/main/java/com/example/user/textschedule/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rvTexts = (RecyclerView) findViewById(R.id.rv_text);
+        /*rvTexts = (RecyclerView) findViewById(R.id.rv_text);*/
 
         //insert data on Recycler View
 
