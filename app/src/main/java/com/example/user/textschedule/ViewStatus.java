b115303d8e75e684d33fd9d23dfedc2385eac8a8
paolomanlapaz <32410303+paolomanlapaz@users.noreply.github.com>
2017-11-15 15:02:14 +0800

    Added Edit Status Activity
    designed narin

---
+++ app/src/main/java/com/example/user/textschedule/ViewStatus.java
@@ -0,0 +1,14 @@
+package com.example.user.textschedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_status);
+
+    }
+}
